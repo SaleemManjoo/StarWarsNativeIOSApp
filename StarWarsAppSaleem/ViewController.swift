d56ff9645f5bf8fcc68b06dbbd07e168d26a1c97
Saleem Manjoo <saleem@touchfoundry.co.za>
2019-07-07 11:04:20 +0200

    Initial Commit

---
+++ StarWarsAppSaleem/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StarWarsAppSaleem
+//
+//  Created by Saleem Manjoo on 2019/07/07.
+//  Copyright © 2019 Saleem Manjoo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
